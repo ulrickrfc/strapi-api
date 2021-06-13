b259cb14796798a3f7f0d77a558879bbb0df1041
ulrickrfc <ulrickrfc@DESKTOP-D2TRRJ5.localdomain>
2021-06-12 23:03:57 -0300

    feat: almost all components created

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '98f1564fc3a2a81ebbf006482c681331'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '53577211-e8b9-44d4-b7dd-81a364d4087f'
+};
